502fb0e0a87c7858e63c1f3045123bc7c0e04711
Mohamed El Hamidi <mel-hami@e2r3p8.1337.ma>
2025-01-18 13:21:29 +0100

    back up

--- main.c
+++ main.c
@@ -1,88 +1,101 @@
 #include "push_swap.h"
 
-void	handle_input(int ac, char **av);
-void	error_msg();
+static void	handle_input(int ac, char **av);
+static void	error_msg(void );
 
-void _f()
+static void _f(void )
 {
 	system ("leaks a.out");
 }
 
-int	main(int argc, char **argv)
+static void _print(int *a, int len)
 {
-	int *a;
+	int	i;
+
+	i = 0;
+	while (++i < len)
+		printf("->%s\n", a);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	*a;
+	int	i;
 
 	if (argc < 2)
 		return (SIG_INVALID_INPUT);
 
 	handle_input(argc, argv);
-    //ft_printf("Yey\n");
-    a = malloc((argc - 1) * sizeof(int));
-    if (!a)
-        return (SIG_ERROR);
-
-
-    free(a);
-    atexit(_f);
+	//ft_printf("Yey\n");
+	a = malloc((argc - 1) * sizeof(int));
+	if (!a)
+		return (SIG_ERROR);
+	i = 0;
+	while (++i < argc)
+		a[i] = atoi(argv[i]);
+	_print(a, argc);
+	free(a);
+	atexit(_f);
+	return (0);
 }
 
-void    handle_input(int ac, char **av)
+static void	handle_input(int ac, char **av)
 {
-    int     i;
-    int     j;
-    int     k;
-    int     flag;
-    char    arg[12];
-    int     is_int;
+	int		i;
+	int		j;
+	int		k;
+	int		flag;
+	char	arg[12];
+	int		is_int;
 
-    i = 0;
-    while (++i < ac)
-    {
-        j = 0;
-        k = 0;
-        flag = 0;
-        ft_bzero(arg, 12);
-        if (av[i][j] == '-' || av[i][j] == '+')
-            arg[k++] = av[i][j++];
-        while (av[i][j])
-        {
-            if (!ft_isdigit(av[i][j]))
-            {
-                error_msg();
-                exit (SIG_INVALID_INPUT);
-            }
-            if (!flag && av[i][j] != '0')
-                flag = 1;
-            if (flag)
-            {
-                arg[k++] = av[i][j];
-                if (k > 11)
-                    break ;
-            }
-            j++;
-        }
-        if (!flag && av[i][j - 1] == '0')
-            is_int = 1;
-        if (k == 1 && (arg[0] == '+' || arg[0] == '-'))
-            is_int = 0;
-        else if (k > 11)
-            is_int = 0;
-        else if (k == 11 && ((arg[0] == '+' && ft_strncmp(arg, "+2147483647", k) > 0)
-            || (arg[0] == '-' && ft_strncmp(arg, "-2147483648", k) > 0)))
-            is_int = 0;
-        else if (k == 10 && ft_strncmp(arg, "2147483647", k) > 0)
-            is_int = 0;
-        else
-            is_int = 1;
-        if (!is_int)
-        {
-                error_msg();
-                exit (SIG_INVALID_INPUT);
-        }
-    }
+	i = 0;
+	while (++i < ac)
+	{
+		j = 0;
+		k = 0;
+		flag = 0;
+		ft_bzero(arg, 12);
+		if (av[i][j] == '-' || av[i][j] == '+')
+			arg[k++] = av[i][j++];
+		while (av[i][j])
+		{
+			if (!ft_isdigit(av[i][j]))
+			{
+				error_msg();
+				exit (SIG_INVALID_INPUT);
+			}
+			if (!flag && av[i][j] != '0')
+				flag = 1;
+			if (flag)
+			{
+				arg[k++] = av[i][j];
+				if (k > 11)
+					break ;
+			}
+			j++;
+		}
+		if (!flag && av[i][j - 1] == '0')
+			is_int = 1;
+		if (k == 1 && (arg[0] == '+' || arg[0] == '-'))
+			is_int = 0;
+		else if (k > 11)
+			is_int = 0;
+		else if (k == 11 && ((arg[0] == '+' && ft_strncmp(arg, "+2147483647", k) > 0)
+				|| (arg[0] == '-' && ft_strncmp(arg, "-2147483648", k) > 0)))
+			is_int = 0;
+		else if (k == 10 && ft_strncmp(arg, "2147483647", k) > 0)
+			is_int = 0;
+		else
+			is_int = 1;
+		if (!is_int)
+		{
+			error_msg();
+			exit (SIG_INVALID_INPUT);
+		}
+	}
 }
 
-void    error_msg()
+static void	error_msg(void )
 {
-    ft_printf("Error\n");
+	ft_printf("Error\n");
 }
--- push_swap.h
+++ push_swap.h
@@ -2,8 +2,10 @@
 # define PUSH_SWAP_H
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdio.h> //debug
 # include "ft_printf.h"
 
 # define SIG_ERROR 1
 # define SIG_INVALID_INPUT 2
+
 #endif
